3fc15a77146b3d03b46c404f8870a287e8a9f00f
1526853 <c.a.restituyonunez@wlv.ac.uk>
2016-12-03 12:53:45 -0400

    First Commit including basic structure of the data models.

---
+++ app/assets/js/SnowScene/Models/models.js
@@ -0,0 +1,17 @@
+var SNOWDAY = {};
+
+SNOWDAY.MODELS = {
+    Flake:function(options){
+        var self = this,
+            _speed = 0,
+            _position = {
+                x:0,
+                y:0
+            },
+            _blurriness = 0;
+        
+        self.render = function(){
+            
+        };
+    }
+};
