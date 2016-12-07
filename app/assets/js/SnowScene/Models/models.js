19ceff41fca8971740dba49e35a8f03a1b4beb10
Cristian Restituyo <cristian.restituyo@sdq.theoreminc.net>
2016-12-07 09:30:51 -0500

    Added test of self-deleting class.

--- app/assets/js/SnowScene/Models/models.js
+++ app/assets/js/SnowScene/Models/models.js
@@ -18,9 +18,8 @@ SNOWDAY.MODELS = {
             _dy = 5,
             _flakeDOM = {},
             _ctx = options.Context || null,
-            _flakeTL = new TimelineMax({repeat:-1,yoyo:true}),
-            _animationDuration = options.animationDuration,
-            _animationClass = options.animationClass;
+            _flakeTL = new TimelineMax({repeat: -1, yoyo: true}),
+            _animationDuration = options.animationDuration
 
         var XOFFSET = 2;
         //Here is where the snowflake is initialized.
@@ -44,16 +43,24 @@ SNOWDAY.MODELS = {
             flakeDOM.className = 'snowflake';
             flakeDOM.style.width = _size + unit;
             flakeDOM.style.height = _size + unit;
-            flakeDOM.style.filter =  'blur('+_size/7+'px)';
+            flakeDOM.style.filter = 'blur(' + _size / 7 + 'px)';
             //flakeDOM.style.opacity = _blur;
             flakeDOM.style.left = _position.x + unit;
             flakeDOM.style.top = _position.y + unit;
             _flakeDOM = flakeDOM;
             _scene.appendChild(_flakeDOM);
 
-            var flakeTM = TweenMax.to($(_flakeDOM),_animationDuration/1000,{y:($(_scene).height()+50)});
-            
-            _flakeTL.fromTo($(_flakeDOM),2,{x:'-=20',ease:Power1.easeInOut},{x:'+=20',ease:Power1.easeInOut},"START");
+            var flakeTM = TweenMax.to($(_flakeDOM), _animationDuration / 1000, {
+                y: ($(_scene).height() + 50)
+            });
+
+            _flakeTL.fromTo($(_flakeDOM), 2, {
+                x: '-=20',
+                ease: Power1.easeInOut
+            }, {
+                x: '+=20',
+                ease: Power1.easeInOut
+            }, "START");
             self.animate();
 
         };
@@ -66,13 +73,17 @@ SNOWDAY.MODELS = {
         };
         //Animation method
         self.animate = function () {
-            
-        var $flake = _flakeDOM;
 
-         setTimeout(function(){
+            var $flake = _flakeDOM;
+
+            setTimeout(function () {
                 $($flake).remove();
-                console.log('removed');
-            },_animationDuration);
+                console.log('removed ' + this);
+                /*******Experiment(Self-deleting class)**********/
+                this = null;
+                delete this;
+                /***************** */
+            }, _animationDuration);
         };
         //Constructor.
         (function () {
@@ -84,24 +95,23 @@ SNOWDAY.MODELS = {
         var self = this,
             _sceneInDOM = options.SceneRef || null;
         var _animationInterval = function () {};
-        var BATCHTIME = 100;
+        var BATCHTIME = 750;
         var FLAKEBATCH = 20;
 
         //Function that generates x amount of snow flakes
         self.letItSnow = function () {
             setInterval(function () {
-             //   for (var x = 0; x < FLAKEBATCH; x++) {
-                    var flake = new SNOWDAY.MODELS.Flake({
-                        Scene: _sceneInDOM,
-                        animationDuration:40000
-                    });
-               // }
+                //   for (var x = 0; x < FLAKEBATCH; x++) {
+                var flake = new SNOWDAY
+                    .MODELS
+                    .Flake({Scene: _sceneInDOM, animationDuration: 30000});
+                // }
             }, BATCHTIME);
 
         };
         //Constructor
         (function () {
-           self.letItSnow();
+            self.letItSnow();
         })();
     }
 };
